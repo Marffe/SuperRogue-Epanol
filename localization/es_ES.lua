b52710cd7e672a82df614d57e137206dafbc6cb9
Marffe <efframlol@gmail.com>
2025-10-29 07:40:25 -0500

    Added Spanish localization

---
+++ localization/es_ES.lua
@@ -0,0 +1,93 @@
+return {
+    descriptions = {
+        Other = {
+            p_sr_mod_booster = {
+                name = 'Paquete de Mods',
+                text = {
+                    "Escoge hasta {C:attention}#1#{} de",
+                    "{C:attention}#2#{C:attention} Mods{} para activar",
+                    "inmediatamente",
+                },
+            },
+        }
+    },
+    misc = {
+        dictionary = {
+            b_sr_activation_mode = 'Modo de Activación',
+            b_sr_activation_threashold = 'Contador de Activación',
+            b_sr_blacklist = 'Lista Negra',
+            b_sr_boosters_in_shop = 'Permitir Paquetes de Mods en la Tienda',
+            b_sr_mods = 'Mods',
+            b_sr_start_with_mod = 'Iniciar la partida con un mod siendo activado por el medio elegido',
+            b_sr_starting_mods = 'Mods Iniciales',
+            b_sr_trigger_type = 'Tipo de Activación',
+            b_sr_main_config_options = 'Configuración Principal',
+            b_sr_optional_config_options = 'Configuración Opcional',
+            b_sr_pack_size = 'Tamaño del Paquete',
+            b_sr_pack_choices = 'Elecciones por Paquete',
+            b_sr_rand_starting = 'Cantidad de mods iniciales',
+            b_sr_vanilla_obj_bl = 'Lista Negra de contenido vanilla',
+
+            k_mod_consumable = 'Mod',
+            k_sr_activation = ' Ha sifo activado',
+            k_sr_mod_booster = 'Escoge un mod',
+            k_sr_mod_not_active_ex = 'Mod desactivado',
+
+            ph_sr_crossed_blacklist = '(Los mods tachados no aparecerán)',
+            ph_sr_crossed_starting = '(Los mods tachados no aparecerán al principio)',
+            ph_sr_mods_activated = 'Mods Activos',
+            ph_sr_mods_blacklist = 'Lista Negra de Mods',
+            ph_sr_mods_starting = 'Mods activos al iniciar',
+            ph_sr_no_mods_available = 'SuperRogue no encontró mods compatibles',
+            ph_sr_no_mods_left = 'No hay más mods que activar',
+            ph_sr_no_mods_run = 'No hay mods activados',
+
+
+            sr_activation_mode_desc = {
+                "Escoge el tipo de",
+                "Activación que deseas usar"
+            },
+            sr_activation_threashold_desc = {
+                "Escoge cuantas Rondas/Apuesta",
+                "se necesitan para activar un mod"
+            },
+            sr_trigger_type_desc = {
+                "Escoge el tipo de condición",
+                "para activar un mod"
+            },
+
+            sr_pack_size_desc = {
+                "Escoge cuantos mods parecerán",
+                "en Paquetes de Mods"
+            },
+            sr_pack_choices_desc = {
+                "Escoge cuantos Mods se pueden",
+                "activar por Paquete"
+            },
+            sr_rand_starting_desc = {
+                "La Cantidad de mods que se activarán",
+                "al azar cuando incies partida",
+            },
+
+
+            sr_activation_mode_options = {
+                "Al azar",
+                "Elección"
+            },
+            sr_trigger_type_options = {
+                "Apuesta",
+                "Ronda"
+            },
+            sr_config_pages = {
+                "Página 1/3",
+                "Página 2/3",
+                "Página 3/3"
+            },
+
+        },
+        v_dictionary = {
+            k_sr_antes_until_next_mod = 'Apuestas necesarias para activar el siguiente mod: #1#',
+            k_sr_rounds_until_next_mod = 'Rondas necesarias para activar el siguiente mod: #1#',
+        }
+    }
+}
